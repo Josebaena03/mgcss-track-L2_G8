366930c4994e24e1df9485bb47dd234f9cbee307
PabloGomez03 <jbarea155@gmail.com>
2026-03-13 13:23:29 +0100

    Feat: Iniciación del proyecto

---
+++ l2g8/src/main/java/com/mgcss/l2g8/L2g8Application.java
@@ -0,0 +1,13 @@
+package com.mgcss.l2g8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L2g8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L2g8Application.class, args);
+	}
+
+}
---
+++ l2g8/src/test/java/com/mgcss/l2g8/L2g8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgcss.l2g8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L2g8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
